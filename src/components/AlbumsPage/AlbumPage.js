3c28ca8c1d3159f9bd73413f9c6b9c326a605c05
vaitkeviciut <indre.vaitkeviciute96@gmail.com>
2023-03-14 10:46:13 +0200

    album page edited

--- src/components/AlbumsPage/AlbumPage.js
+++ src/components/AlbumsPage/AlbumPage.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
+import { firstLetterUpperCase } from '../functions.js'
 import PageWrapper from '../PageWrapper/PageWrapper';
 import axios from 'axios';
 
+import './AlbumPage.scss';
+
 const AlbumPage = () => {
     const { albumId } = useParams();
+    const [photos, setPhotos] = useState([]);
     const [title, setTitle] = useState('');
     const [name, setName] = useState('');
     const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [date, setDate] = useState('');
     const [time, setTime] = useState('');
 
@@ -20,23 +25,45 @@ const AlbumPage = () => {
         setTitle(albumsData.title)
         setName(albumsData.name)
         setUsername(albumsData.username)
+        setEmail(albumsData.email)
         setDate(albumsData.date)
         setTime(albumsData.time)
       });
     }, [albumId]);
 
+    useEffect(() => {
+      axios.get(`http://localhost:3000/albums/1/photos`).then((photos) => {
+        const photosData = photos.data;
+        setPhotos(photosData);
+      });
+    }, []);
+
 
   return (
     <PageWrapper>
       <div className='album-wrapper'>
-
+        
             <div className='album-content'>
-              <h2 className='album-title'>{title}</h2>
-              <span>{name}</span>
-              <span>{username}</span>
-              <span>{date}</span>
-              <span>{time}</span>
+              <h2 className='album-title'>{firstLetterUpperCase(title)}</h2>
+              <div className='user-wrapper'>
+                <span className='name'>by {name} ({username})</span>
+                <span className='email'>{email}</span>
+              </div>
+              <div className='date-time-wrapper'>
+                <span className='date'>{date}</span>
+                <span className='time'>{time}</span>
+              </div>
             </div>
+
+            {photos && photos.length > 0 ? (
+            <div className="photos-wrapper">
+              {photos.map((photo, index) => (
+                <img key={index} src={photo.thumbnailUrl} />
+              ))}
+            </div>
+          ) : (
+            <p>No photos</p>
+          )}
     
 
       </div>
